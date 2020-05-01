0a9ae89f96a361449e53d0f5bf0d399322585b03
David Logacho Torres <davidlogacho13@outlook.com>
2020-05-01 17:51:49 -0500

    Cambio a Usuarios

--- server.py
+++ server.py
@@ -2,17 +2,19 @@ from opcua import Server
 from random import randint
 import datetime
 import time
+from opc_server_security import OPC_SERVER_SECURITY
 
+opc_server = OPC_SERVER_SECURITY()
 server = Server()
 
 url = "opc.tcp://192.168.100.6:4840"
 server.set_endpoint(url)
 
-server.set_security_policy([ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt])
+#server.set_security_policy([ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt])
 # load server certificate and private key. This enables endpoints
 # with signing and encryption.
-server.load_certificate("/home/administrador/opc/opcserver/my_cert.der")
-server.load_private_key("/home/administrador/opc/opcserver/my_cert/my_cert.pem")
+#server.load_certificate("/home/administrador/opc/opcserver/my_cert.der")
+#server.load_private_key("/home/administrador/opc/opcserver/my_private_key.pem")
 
 name = "OPCUA_SERVER"
 addspace = server.register_namespace(name)
@@ -31,11 +33,18 @@ Time.set_writable()
 
 server.start()
 print("Server at {}".format(url))
+opc_server.init_opc_server_security('192.168.100.6')
+opc_server.set_server_credentials('admin', 'admin')
 
 while True:
-    Temperature = randint(10,50)
-    Pressure = randint(20, 999)
-    TIME = datetime.datetime.now()
+
+    res = opc_server.client_authentication()
+    
+    while res:
+        Temperature = randint(10,50)
+        Pressure = randint(20, 999)
+        TIME = datetime.datetime.now()
+    
 
     print (Temperature, Pressure, TIME)
 
