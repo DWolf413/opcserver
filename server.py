5bc9c0cc00ba13be843f09ccc31f71070745747b
David Logacho Torres <davidlogacho13@outlook.com>
2020-05-01 20:14:27 -0500

    Agragado inicio del programa

--- server.py
+++ server.py
@@ -3,44 +3,45 @@ from random import randint
 import datetime
 import time
 
-server = Server()
+if __name__ == '__main__':
+    server = Server()
 
-url = "opc.tcp://192.168.100.6:4840"
-server.set_endpoint(url)
+    url = "opc.tcp://192.168.100.6:4840"
+    server.set_endpoint(url)
 
-server.set_security_policy([ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt])
-server.load_certificate("/home/administradpr/opc/opcserver/certificados/cert.der")
-server.load_private_key("/home/administradpr/opc/opcserver/certificados/private_key.pem")
+    server.set_security_policy([ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt])
+    server.load_certificate("/home/administradpr/opc/opcserver/certificados/cert.der")
+    server.load_private_key("/home/administradpr/opc/opcserver/certificados/private_key.pem")
 
-name = "OPCUA_SERVER"
-addspace = server.register_namespace(name)
+    name = "OPCUA_SERVER"
+    addspace = server.register_namespace(name)
 
-node = server.get_objects_node()
+    node = server.get_objects_node()
 
-Param = node.add_object(addspace, "Parameters")
+    Param = node.add_object(addspace, "Parameters")
 
-Temp = Param.add_variable(addspace, "Tempreature", 0)
-Press = Param.add_variable(addspace, "Pressure", 0)
-Time = Param.add_variable(addspace, "Time", 0)
+    Temp = Param.add_variable(addspace, "Tempreature", 0)
+    Press = Param.add_variable(addspace, "Pressure", 0)
+    Time = Param.add_variable(addspace, "Time", 0)
 
-Temp.set_writable()
-Press.set_writable()
-Time.set_writable()
+    Temp.set_writable()
+    Press.set_writable()
+    Time.set_writable()
 
-server.start()
-print("Server at {}".format(url))
+    server.start()
+    print("Server at {}".format(url))
 
-while True:
-    Temperature = randint(10,50)
-    Pressure = randint(20, 999)
-    TIME = datetime.datetime.now()
+    while True:
+        Temperature = randint(10,50)
+        Pressure = randint(20, 999)
+        TIME = datetime.datetime.now()
 
-    print (Temperature, Pressure, TIME)
+        print (Temperature, Pressure, TIME)
 
-    Temp.set_value(Temperature)
-    Press.set_value(Pressure)
-    Time.set_value(TIME)
+        Temp.set_value(Temperature)
+        Press.set_value(Pressure)
+        Time.set_value(TIME)
 
-    time.sleep(2)
+        time.sleep(2)
 
 
