9c766af58903e2ce581acb6ca5caac6ebbc19b1d
David Logacho Torres <davidlogacho13@outlook.com>
2020-05-17 17:32:17 -0500

    Agregado trama

--- cliente_socket.py
+++ cliente_socket.py
@@ -4,7 +4,7 @@ import json
 HOST = 'localhost'
 PORT = 8000
 
-my_dict = {"Name":"Bob", "age":39}
+my_dict = {'Extension': '9000', 'Interfono': 'SEP00505629E136', 'Estacion': '', 'Registro': False, 'id': 'SEP00505629E136', 'Llamada': 'NO CONECTADO', 'Linea': 'L01', 'Estado': 'REPOSO'}
 jsn = json.dumps(my_dict)
 print(jsn)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
